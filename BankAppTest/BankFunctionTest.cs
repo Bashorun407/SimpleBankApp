786c047b7e54aa5901c878bc4a535ea922aa2a08
Oluwaseyi Akinbobola <83551336+Bashorun407@users.noreply.github.com>
2023-07-25 15:47:49 +0100

    Committing all updated files to local repository

---
+++ BankAppTest/BankFunctionTest.cs
@@ -0,0 +1,26 @@
+using BankApp.Methods;
+
+namespace BankAppTest
+{
+    [TestClass]
+    public class BankFunctionTest
+    {
+        [TestMethod]
+        public void DepositFundsTest()
+        {
+            AccountTransactionMethods service = new();
+        }
+
+        [TestMethod]
+        public void WithFundsTest()
+        {
+            AccountTransactionMethods service = new();
+        }
+
+        [TestMethod]
+        public void TransferFundsTest()
+        {
+            AccountTransactionMethods service = new();
+        }
+    }
+}
